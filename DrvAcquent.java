f03ecb10abf7b4178fe78b66b2e11c5eeea72b9c
jeevansonata <jeevansonata@users.noreply.github.com>
2015-02-27 10:29:18 +0530

    Update DrvAcquent.java
    Updated to demo Acq code 27feb

--- DrvAcquent.java
+++ DrvAcquent.java
@@ -8,29 +8,24 @@ public class DrvAcquent extends SonnetTestNGBase{
 	private CreateRFQ create = new CreateRFQ();
 	private SearchRFQ search = new SearchRFQ();
 	
-    @Test(description="Create the RFQ",priority=1)
+//For RFQ Description Mandatory feild Blank
+    @Test(description="Create the RFQ",priority=1,enabled=true)
 	public void CreateRFQmethod() throws Exception{
-    	create.initTestData();
-		create.acquentLogin();
+    	    create.initTestData();
+	    create.acquentLogin();
+	    create.requestForQuote(1);////For RFQ Description Mandatory feild Blank
+	    create.errorRFQ();
+	    create.requestForQuote(2);////For Unit Description Mandatory feild Blank
+	    create.errorUnit();
+	    create.requestForQuote(3);////For RFQ Description Mandatory feild Blank
+	    create.successmsg();
+	    
+	   
+     }   
 
-	   create.requestForQuote();
-		
-	/*	create.parts();
-		create.suppliers();
-		create.publish();
-		create.analyze();
-	*/
-    	
-    
-	}
 
-	@Test(description="Search  the RFQ", priority=2)
-	public void SearchRFQmethod() {
-	
+	    
 
-		/*search.enterRFQsearchDetails();
-*/
-	}
 	
 	
 
